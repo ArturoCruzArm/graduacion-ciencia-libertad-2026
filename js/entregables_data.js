9cec2a972a1469cddb92669edc93cd8704d38fee
Arturo Cruz <foro7.arturocruz@gmail.com>
2026-07-01 17:32:06 -0600

    Add entregables page with download buttons for edited photos

---
+++ js/entregables_data.js
@@ -0,0 +1,94 @@
+var ENTREGABLES = {
+  "4x6 SI": {
+    "base": "ciencia y libertad/4x6/SI/",
+    "fotos": [
+      "DSC_9696Nigromante4x6.webp",
+      "DSC_9697Nigromante4x6.webp",
+      "DSC_9698Nigromante4x6.webp",
+      "DSC_9700Nigromante4x6.webp",
+      "DSC_9702Nigromante4x6.webp",
+      "DSC_9704Nigromante4x6.webp",
+      "DSC_9707Nigromante4x6.webp",
+      "DSC_9709Nigromante4x6.webp",
+      "DSC_9710Nigromante4x6.webp",
+      "DSC_9712Nigromante4x6.webp",
+      "DSC_9715Nigromante4x6.webp",
+      "DSC_9718Nigromante4x6.webp",
+      "DSC_9720Nigromante4x6.webp",
+      "DSC_9723Nigromante4x6.webp",
+      "DSC_9725Nigromante4x6.webp",
+      "DSC_9730Nigromante4x6.webp",
+      "DSC_9731Nigromante4x6.webp",
+      "DSC_9735Nigromante4x6.webp",
+      "DSC_9738Nigromante4x6.webp",
+      "DSC_9742Nigromante4x6.webp",
+      "DSC_9744Nigromante4x6.webp",
+      "DSC_9746Nigromante4x6.webp",
+      "DSC_9749Nigromante4x6.webp",
+      "DSC_9751Nigromante4x6.webp",
+      "DSC_9752Nigromante4x6.webp",
+      "DSC_9755Nigromante4x6.webp"
+    ]
+  },
+  "4x6 NO": {
+    "base": "ciencia y libertad/4x6/NO/",
+    "fotos": [
+      "DSC_9693Nigromante4x6.webp",
+      "DSC_9694Nigromante4x6.webp",
+      "DSC_9695Nigromante4x6.webp",
+      "DSC_9699Nigromante4x6.webp",
+      "DSC_9701Nigromante4x6.webp",
+      "DSC_9703Nigromante4x6.webp",
+      "DSC_9705Nigromante4x6.webp",
+      "DSC_9708Nigromante4x6.webp",
+      "DSC_9711Nigromante4x6.webp",
+      "DSC_9713Nigromante4x6.webp",
+      "DSC_9714Nigromante4x6.webp",
+      "DSC_9716Nigromante4x6.webp",
+      "DSC_9717Nigromante4x6.webp",
+      "DSC_9719Nigromante4x6.webp",
+      "DSC_9721Nigromante4x6.webp",
+      "DSC_9722Nigromante4x6.webp",
+      "DSC_9724Nigromante4x6.webp",
+      "DSC_9727Nigromante4x6.webp",
+      "DSC_9728Nigromante4x6.webp",
+      "DSC_9729Nigromante4x6.webp",
+      "DSC_9732Nigromante4x6.webp",
+      "DSC_9733Nigromante4x6.webp",
+      "DSC_9734Nigromante4x6.webp",
+      "DSC_9736Nigromante4x6.webp",
+      "DSC_9737Nigromante4x6.webp",
+      "DSC_9739Nigromante4x6.webp",
+      "DSC_9740Nigromante4x6.webp",
+      "DSC_9743Nigromante4x6.webp",
+      "DSC_9745Nigromante4x6.webp",
+      "DSC_9747Nigromante4x6.webp",
+      "DSC_9748Nigromante4x6.webp",
+      "DSC_9750Nigromante4x6.webp",
+      "DSC_9754Nigromante4x6.webp",
+      "DSC_9756Nigromante4x6.webp",
+      "DSC_9757Nigromante4x6.webp",
+      "DSC_9758Nigromante4x6.webp"
+    ]
+  },
+  "6x8": {
+    "base": "ciencia y libertad/6x8/",
+    "fotos": [
+      "cyl6.webp",
+      "cylm.webp"
+    ]
+  },
+  "Pen Drive": {
+    "base": "ciencia y libertad/pd/",
+    "fotos": [
+      "DSC_9623seundaria 3c4x6.webp",
+      "DSC_9624seundaria 3c4x6.webp",
+      "DSC_9626seundaria 3c4x6.webp",
+      "DSC_9629seundaria 3c4x6.webp",
+      "DSC_9631seundaria 3c4x6.webp",
+      "DSC_9635seundaria 3c4x6.webp",
+      "DSC_9637seundaria 3c4x6.webp",
+      "DSC_9642seundaria 3c4x6.webp"
+    ]
+  }
+};
